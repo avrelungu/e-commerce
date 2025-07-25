b5a05f427bb1478fc012a49f4c3463551a4dbe8a
Aurel Lungu <aurel.lungu.dev@gmail.com>
2025-07-25 15:33:20 +0300

    added Kafka config files for Order Service

--- order_service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order_service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.order_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class OrderServiceApplication {
@@ -10,4 +13,10 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("orders", "Hello kafkaa");
+		};
+	}
 }
