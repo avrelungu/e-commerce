b3016caba04aea87d0e70c111172014029fe5454
Aurel Lungu <aurel.lungu.dev@gmail.com>
2025-08-19 16:24:59 +0300

    updated application.properties to application.yml and added necessary configuration

---
+++ shipping-service/src/main/java/com/example/shipping_service/config/KafkaTopics.java
@@ -0,0 +1,18 @@
+package com.example.shipping_service.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "kafka.topics")
+@Component
+@Data
+public class KafkaTopics {
+    private String orderCreated;
+    private String stockReserved;
+    private String stockReleased;
+    private String outOfStock;
+    private String paymentRequest;
+    private String paymentProcessed;
+    private String paymentFailed;
+}
